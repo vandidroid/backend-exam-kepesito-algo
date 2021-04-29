42e1fac6a617b046bb6c06733cc4b4ecb9606fbf
Andi <1vandidroid1@gmail.com>
2021-04-29 10:14:04 +0200

    Implement method

--- src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
@@ -1,5 +1,19 @@
 package hu.nive.ujratervezes.adddigits;
 
 public class AddDigits {
-    //todo: implement your solution here
+    public int addDigits(String input) {
+        int result = 0;
+
+        if (input == null || input.length() == 0) return -1;
+
+        for (int i=0; i<input.length(); i++) {
+            try {
+                result += Integer.parseInt(String.valueOf(input.charAt(i)));
+            } catch (NumberFormatException e) {
+
+            }
+        }
+
+        return result;
+    }
 }
